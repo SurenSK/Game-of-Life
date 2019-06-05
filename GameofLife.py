bb45fdd1a923264f785013224cbbd20920b73a83
SurenSK <kumar.s.suren@gmail.com>
2019-06-05 07:35:04 -0400

    More optimizations

--- GameofLife.py
+++ GameofLife.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import math
+from typing import List
 
 BLACK, GRAY, WHITE = (0, 0, 0), (128, 128, 128), (255, 255, 255)
 RED, GREEN, BLUE = (255, 0, 0), (0, 255, 0), (0, 0, 255)
@@ -12,6 +13,8 @@ CELL_WIDTH, CELL_HEIGHT = 5, 5
 BUFFER_SIZE = 1
 N_X = math.floor((BOARD_WIDTH-BUFFER_SIZE)/(CELL_WIDTH+BUFFER_SIZE))
 N_Y = math.floor((BOARD_HEIGHT-BUFFER_SIZE)/(CELL_HEIGHT+BUFFER_SIZE))
+TOT_CELL_W = BUFFER_SIZE + CELL_WIDTH
+TOT_CELL_H = BUFFER_SIZE + CELL_HEIGHT
 
 pygame.init()
 pygame.display.set_caption("Game of Life")
@@ -27,7 +30,7 @@ def board_print(_board: list) -> None:
     board_state = ""
     for x in range(N_X):
         for y in range(N_Y):
-            board_state += str(_board[x][y])
+            board_state += str(_board[x][y]==1,", ")
             # print(_board[x][y], sep='', end='')
         board_state += "\r\n"
         # print("")
@@ -37,7 +40,7 @@ def board_print(_board: list) -> None:
 
 
 # Just cuts off at board edges
-def new_board_flat_moore(old_board: list) -> list:
+def simulate_flat_moore(old_board: list) -> list:
     new_board = [[0 for x in range(N_X)] for y in range(N_Y)]
     for x in range(N_X):
         for y in range(N_Y):
@@ -51,14 +54,15 @@ def new_board_flat_moore(old_board: list) -> list:
 
 
 # Wrap-around / Toriodal Topography
-# ~20ms for 961x961 board w/ 5x5 cell w/ 1 buffer
-def new_board_toroidal_moore(old_board: list) -> list:
+# ~2ms for 961x961 board w/ 5x5 cell w/ 1 buffer
+def simulate_toroidal_moore(old_board: list) -> list:
     new_board = [[False for x in range(N_X)] for y in range(N_Y)]
     for x in range(N_X):
+        col_x = BUFFER_SIZE + (TOT_CELL_W * x)
+        neg_x = x - 1
+        pos_x = (x + 1) % N_X
         for y in range(N_Y):
-            pos_x = (x + 1) % N_X
             pos_y = (y + 1) % N_Y
-            neg_x = x - 1
             neg_y = y - 1
             n_neighbors = old_board[x][neg_y] + old_board[x][pos_y] + \
                 old_board[neg_x][neg_y] + old_board[neg_x][y] + old_board[neg_x][pos_y] + \
@@ -70,35 +74,30 @@ def new_board_toroidal_moore(old_board: list) -> list:
                 continue
             elif n_neighbors == 3:
                 new_board[x][y] = True
+                if old_board[x][y] is False:
+                    pygame.draw.rect(screen, ON_COLOR, [col_x, BUFFER_SIZE + (TOT_CELL_H * y), CELL_WIDTH, CELL_HEIGHT])
             elif old_board[x][y] is True:
                 new_board[x][y] = True
+                pygame.draw.rect(screen, ON_COLOR, [col_x, BUFFER_SIZE + (TOT_CELL_H * y), CELL_WIDTH, CELL_HEIGHT])
     return new_board
-
-
-# ~40ms for 961x961 board w/ 5x5 cell w/ 1 buffer
-def draw_new_board(_board: list) -> None:
-    for x in range(N_X):
-        for y in range(N_Y):
-            pygame.draw.rect(screen, (ON_COLOR, OFF_COLOR)[n_board[y][x] is False],
-                             [BUFFER_SIZE + (BUFFER_SIZE + CELL_WIDTH) * x,
-                              BUFFER_SIZE + (BUFFER_SIZE + CELL_HEIGHT) * y,
-                              CELL_WIDTH, CELL_HEIGHT])
 
 
 done = False
 avg_fps = 0
+i = 0
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    screen.fill(GRID_COLOR)
-    draw_new_board(n_board)
+    screen.fill(OFF_COLOR)
+    n_board = simulate_toroidal_moore(n_board)
     pygame.display.flip()
     avg_fps += clock.get_fps()
-    avg_fps /= 2
-    n_board = new_board_toroidal_moore(n_board)
-    clock.tick(60)
+    i += 1
+    done = i == 1000
+    print(clock.get_fps())
+    clock.tick()
 
-print("Avg FPS", avg_fps)
+print("Avg FPS", avg_fps/i)
 print("Exiting...")
