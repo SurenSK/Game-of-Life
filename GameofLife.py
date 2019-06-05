68c4553e1e3ffe1a4d75fa53113e816fdf6d4e16
SurenSK <kumar.s.suren@gmail.com>
2019-06-05 02:04:49 -0400

    Got torus mode working

--- GameofLife.py
+++ GameofLife.py
@@ -7,11 +7,12 @@ RED, GREEN, BLUE = (255, 0, 0), (0, 255, 0), (0, 0, 255)
 GRID_COLOR = GRAY
 OFF_COLOR = WHITE
 ON_COLOR = BLACK
-BOARD_WIDTH, BOARD_HEIGHT = 951, 951
+BOARD_WIDTH, BOARD_HEIGHT = 961, 961
 CELL_WIDTH, CELL_HEIGHT = 5, 5
 BUFFER_SIZE = 1
 N_X = math.floor((BOARD_WIDTH-BUFFER_SIZE)/(CELL_WIDTH+BUFFER_SIZE))
 N_Y = math.floor((BOARD_HEIGHT-BUFFER_SIZE)/(CELL_HEIGHT+BUFFER_SIZE))
+screen = pygame.display.set_mode((BOARD_WIDTH, BOARD_HEIGHT))
 
 
 # Just cuts off at board edges
@@ -28,6 +29,28 @@ def calc_new_board(old_board: list) -> list:
     return new_board
 
 
+# Wrap-around / Toriodal Topography
+def calc_new_board_torus(old_board: list) -> list:
+    new_board = [[0 for x in range(N_X)] for y in range(N_Y)]
+    for x in range(N_X):
+        for y in range(N_Y):
+            pos_x = (x + 1) % N_X
+            pos_y = (y + 1) % N_Y
+            neighbors = [
+                old_board[x - 1][y - 1],
+                old_board[x - 1][y],
+                old_board[x - 1][pos_y],
+                old_board[x][y - 1],
+                old_board[x][pos_y],
+                old_board[pos_x][y - 1],
+                old_board[pos_x][y],
+                old_board[pos_x][pos_y],
+            ]
+            n_neighbors = sum(neighbors) * (1, -1)[old_board[x][y] == 0]
+            new_board[x][y] = 1 if n_neighbors == -3 or n_neighbors == 2 or n_neighbors == 3 else 0
+    return new_board
+
+
 def board_print(_board: list) -> None:
     for x in range(N_X):
         for y in range(N_Y):
@@ -36,33 +59,41 @@ def board_print(_board: list) -> None:
     print("-" * N_X)
 
 
+def draw_new_board(_board: list) -> None:
+    for x in range(N_X):
+        for y in range(N_Y):
+            pygame.draw.rect(screen, (ON_COLOR, OFF_COLOR)[n_board[y][x] == 0],
+                             [BUFFER_SIZE + (BUFFER_SIZE + CELL_WIDTH) * x,
+                              BUFFER_SIZE + (BUFFER_SIZE + CELL_HEIGHT) * y,
+                              CELL_WIDTH, CELL_HEIGHT])
+
+
 pygame.init()
 pygame.display.set_caption("Game of Life")
-screen = pygame.display.set_mode((BOARD_WIDTH, BOARD_HEIGHT))
 clock = pygame.time.Clock()
 
-board = [[random.randint(0, 1) for x in range(N_X)] for y in range(N_Y)]
-# board = [[0 for x in range(N_X)] for y in range(N_Y)]
+n_board = [[random.randint(0, 1) for x in range(N_X)] for y in range(N_Y)]
 
 done = False
+i = 0
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-#        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
+            draw_new_board(n_board)
+            pygame.display.flip()
+            n_board = calc_new_board(n_board)
 #            board = calc_new_board(board)
 #            board_print(board)
 
     screen.fill(GRID_COLOR)
 #    board_print(board)
-    for x in range(N_X):
-        for y in range(N_Y):
-            pygame.draw.rect(screen, (ON_COLOR, OFF_COLOR)[board[y][x] == 0],
-                             [BUFFER_SIZE + (BUFFER_SIZE + CELL_WIDTH) * x,
-                              BUFFER_SIZE + (BUFFER_SIZE + CELL_HEIGHT) * y,
-                              CELL_WIDTH, CELL_HEIGHT])
+    draw_new_board(n_board)
     pygame.display.flip()
-    board = calc_new_board(board)
-    clock.tick(12)
+#    n_board = calc_new_board(n_board)
+    n_board = calc_new_board_torus(n_board)
+#    print(clock.get_fps())
+    clock.tick(60)
 
 print("Exiting...")
