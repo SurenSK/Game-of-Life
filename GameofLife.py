063b0f0cf844a08a807314a013932914e80f7c1e
SurenSK <kumar.s.suren@gmail.com>
2019-07-18 21:20:06 -0400

    Marginally improved performance by ~1.2x by rewriting convolution

--- GameofLife.py
+++ GameofLife.py
@@ -13,6 +13,7 @@ BLACK, GRAY, WHITE = (0, 0, 0), (128, 128, 128), (255, 255, 255)
 ON_COLOR = BLACK
 OFF_COLOR = WHITE
 BOARD_W, BOARD_H = 1920, 1080
+RESOLUTION_SCALE = 1
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -21,22 +22,28 @@ screen = pygame.display.set_mode((BOARD_W, BOARD_H), depth=8)
 pygame.display.set_palette([OFF_COLOR]+[ON_COLOR]+[BLUE]+[GREEN]+[RED])
 
 kernel_h = np.array([1, 1, 1])
-board = np.random.choice(a=[0, 1], size=(BOARD_W, BOARD_H))
-n_neighbors = np.zeros(shape=(BOARD_W, BOARD_H), dtype=np.dtype(np.int8))
+CELLS_W, CELLS_H = BOARD_W//RESOLUTION_SCALE, BOARD_H//RESOLUTION_SCALE
+board = np.random.choice(a=[0, 1], size=(CELLS_W, CELLS_H))
+n_neighbors = np.zeros(shape=(CELLS_W, CELLS_H), dtype=np.dtype(np.int8))
+# board = np.random.choice(a=[0, 1], size=(CELLS_W+2, CELLS_H+2))
+# n_neighbors = np.zeros(shape=(CELLS_W+2, CELLS_H+2), dtype=np.dtype(np.int8))
 
 
 def step_toroidal_moore():
     global n_neighbors, board
-    n_neighbors = np.subtract(
-        ndimage.convolve1d(ndimage.convolve1d(board, kernel_h, axis=0, mode="wrap"), kernel_h, axis=1, mode="wrap"),
-        board)
+    board[0, :] = board[-2, :]
+    board[:, 0] = board[:, -2]
+    board[-1, :] = board[1, :]
+    board[:, -1] = board[:, 1]
+    n_neighbors[:, 1:-1] = board[:, :-2] + board[:, 2:] + board[:, 1:-1]
+    n_neighbors[1:-1, :] += n_neighbors[:-2, :] + n_neighbors[2:, :] - board[1:-1, :]
     board[n_neighbors != 2] = 0
     board[n_neighbors == 3] = 1
 
 
 def draw_board():
-    pygame.surfarray.blit_array(screen, board)
-    # ~0.8ms line cost at 1920x1080
+    # board_c = board[1:-1, 1:-1]
+    pygame.surfarray.blit_array(screen, np.repeat(np.repeat(board, RESOLUTION_SCALE, axis=0), RESOLUTION_SCALE, axis=1))
     pygame.display.flip()
 
 
@@ -52,6 +59,16 @@ def profile_function(function, n_trials=3, n_per_trial=10):
     print("Frame {:3d} : {:24s} ~{:.2f}ms".format(frame_n, function+"()", 1000 * min(times)/n_per_trial))
 
 
+def step_frame():
+    global frame_n
+    frame_n += 1
+    step_toroidal_moore()
+    # step_toroidal_moore()
+    # profile_function("draw_board")
+    draw_board()
+    # status_print(frame_n, 100)
+
+
 # ~2.5ms overhead
 frame_n = 0
 required_frames = 999
@@ -59,15 +76,11 @@ print("\nStarting clock...")
 time_start = time.time()
 done = False
 while not done:
-    frame_n += 1
     done = frame_n > required_frames
     for event in pygame.event.get():
         done = True if event.type == pygame.QUIT else False
-    profile_function("step_toroidal_moore")
-    #step_toroidal_moore()
-    profile_function("draw_board")
-    #draw_board()
-    status_print(frame_n, 100)
+        # if event.type == pygame.MOUSEBUTTONDOWN:
+    step_frame()
     clock.tick()
 
 time_end = time.time()
